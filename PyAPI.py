c6768afab4bfafd49949f059f767c7414b297c3d
ThePhilosopher37 <75205458+ThePhilosopher37@users.noreply.github.com>
2021-06-21 12:43:41 +0530

    working model 1.0

--- PyAPI.py
+++ PyAPI.py
@@ -21,7 +21,7 @@ def info (url):
         if(point == []):  
             # repos.append(None)
             break
-        else: 
+        else:
             listData.append(point)
     return listData
 
@@ -37,17 +37,17 @@ print (" BioData : " + (data['bio'] if data['bio'] != None else "NULL"))
 
 print (" Followers : [" + str(data['followers']) + "]")
 if (data['followers'] != 0):
-    followers = info (f"https://api.github.com/users/Fifirex/followers")
+    followers = info (f"https://api.github.com/users/{user}/followers")
     for page in followers:
         for cont in page:
             try:
                 print (" * " + cont['login'])
             except:
                 pass
-            
+
 print (" Following : [" + str(data['following']) + "]")
 if (data['followers'] != 0):
-    following = info (f"https://api.github.com/users/Fifirex/following")
+    following = info (f"https://api.github.com/users/{user}/following")
     for page in following:
         for cont in page:
             try:
@@ -55,15 +55,35 @@ if (data['followers'] != 0):
             except:
                 pass
 
-repos = info (f"https://api.github.com/users/{user}/repos")
+resp = input(" Enquire about Repos? (y/n): ")
+while (resp!= "n" and resp!= "y" and resp!= "N" and resp!= "Y"):
+    print(" Invalid")
+    resp = input(" Enter the choice again (y/n): ")
+if (resp == "n" or resp == "N"):
+    sys.exit(" Thank you")
+else:
+    name = input (" Enter the repo name : ")
+    repos = info (f"https://api.github.com/users/{user}/repos")
+    for page in repos:
+        for cont in page:
+            if(cont['full_name'].split("/")[1] == name):
+                print (" Private : " + ("True" if cont['private'] else "False"))
+                print (" Fork Count : " + str(cont['forks_count']))
+                print (" Stargazers Count : " + str(cont['stargazers_count']))
+                print (" Watchers Count : " + str(cont['watchers_count']))
+            else:
+                pass
 
-all_repo_names=[]
-for page in repos:
-    for repo in page:
-        try:
-            all_repo_names.append(repo['full_name'].split("/")[1])
-        except:
-            pass
+
+# repos = info (f"https://api.github.com/users/{user}/repos")
+
+# all_repo_names=[]
+# for page in repos:
+#     for repo in page:
+#         try:
+#             all_repo_names.append(repo['full_name'].split("/")[1])
+#         except:
+#             pass
 
 # print (all_repo_names)
 # print (len(all_repo_names))
